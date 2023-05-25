90a4363e8e5834e975f62683a8150fe68877f823
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-05-25 17:04:15 +0200

    Initial commit. Started styling, added icon and sun + animation.

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import icon from './assets/images/logo.png';
+import sun from './assets/images/sun.svg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        { <img src={icon} className="App-logo" alt="logo" />}
+      </header>
+      <div className="Sun">
+        <div>
+          {<img src={sun} className="Sun-logo" alt="sun" />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
